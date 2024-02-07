2dcb808f43806ec005470c394757f27dacc167e4
Essam_ElBadawy <essames949@gmail.com>
2024-02-07 19:33:23 +0200

    Add project files.

---
+++ Bookify.Web/Seeds/DefaultUsers.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Bookify.Web.Seeds
+{
+    public static class DefaultUsers
+    {
+        public static async Task SeedAdminUserAsync(UserManager<ApplicationUser> userManager)
+        {
+            ApplicationUser admin = new()
+            {
+                UserName = "Essam",
+                Email = "essames949@gmail.com",
+                FullName = "Essam",
+                EmailConfirmed = true
+            };
+
+            var user = await userManager.FindByEmailAsync(admin.Email);
+
+            if(user is null)
+            {
+                await userManager.CreateAsync(admin, "Asd@1234");
+                await userManager.AddToRoleAsync(admin, AppRoles.Admin);
+            }
+        }
+    }
+}
